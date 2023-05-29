bc575972a5f2e0e75d0140b88016ec7a1d8b97ca
rayan <rayan1.1421@gmail.com>
2023-05-29 22:19:05 +0300

    Add project files.

---
+++ homework/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using homework.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace homework.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(double first, double second)
+
+        {
+            var model = Tuple.Create(first, second);
+            return View(model);
+
+        }
+    }
+}
